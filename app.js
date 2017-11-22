0a0e10b570b804ccc43d9b6b3a0b7f8153d93fce
micheledh <debrishue.michele@gmail.com>
2017-11-22 21:20:03 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+	var email = $("#email");
+	var password = $("#password");
+	var form = $("#formContainer");
+	var message = $("#message");
+	var submitted = false;
+
+	function showMissingValue() {
+		message.css("color", "red");
+		message.text("Please enter your email address and password.");
+	}
+
+	function handleSuccess() {
+		submitted = false;
+		message.css("color", "green");
+		message.text("Login successful");
+	}
+
+	function handleBadCredentials() {
+		submitted = false;
+		message.css("color", "red");
+		message.text("Email address or Password is wrong");
+	}
+
+	function handleDefaultError() {
+		submitted = false;
+		message.css("color", "red");
+		message.text("An error happened. Please try again later.");
+	}
+
+	function handleAllErrors(jqXhr) {
+		if (jqXhr.status === 422) {
+			handleBadCredentials();
+		} else {
+			handleDefaultError();
+		}
+	}
+
+	function makeBody() {
+		return {
+			email: email.val(),
+			password: password.val()
+		};
+	}
+
+	function callLogin(event) {
+		event.preventDefault();
+		var body = makeBody();
+		console.log(body);
+		console.log(submitted);
+		if (body.email && body.password && !submitted) {
+			submitted = true;
+			$.ajax({
+				url: "https://jsonplaceholder.typicode.com/posts",
+				method: "POST",
+				data: makeBody(),
+				dataType: "json",
+				success: handleSuccess,
+				error: handleAllErrors
+			});
+		} else if (!submitted) {
+			showMissingValue();
+		}
+	}
+
+	form.submit(callLogin);
+});
